591ba1469da962ae5938b6d1b26167a38df17d47
Paige Knowles <paknowles@student.fullsail.edu>
2024-03-22 19:20:41 -0400

    user tests update

---
+++ index.test.js
@@ -0,0 +1,4 @@
+const userTests = require('@pknowles-fs/tests')
+
+
+
--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userCtlr = require('../controllers/user');
-const { validateName, validateEmail, validatePhone, validateBirthday, validatePassword } = require('../middleware/utils');
+const { validateName, validateEmail, validatePhone, validateBirthday, validatePassword } = require('@pknowles-fs/utils/index');
+
 
 router.post('/signup', validateName, validateEmail, validatePhone, validateBirthday, validatePassword, userCtlr.signup);
 
