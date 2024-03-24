107f7fc9043553fc87c793e17366004406e52d44
Paige Knowles <paknowles@student.fullsail.edu>
2024-03-24 11:42:15 -0400

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,28 @@
+// const signup = (req, res) => {
+//     const maskedPassword = req.body.password.split("").map(char => "●").join("");
+//     res.status(200).json({
+//         status: true, 
+//         message: 'User has sucessfully signed up!',
+//         ...req.body,
+//         password: maskedPassword
+//     });
+// }
+
+// module.exports = { signup };
+
 const signup = (req, res) => {
+    if (!req.body.password) {
+        return res.status(422).json({
+            status: false,
+            message: 'Password field is missing in the request body.'
+        });
+    }
+
     const maskedPassword = req.body.password.split("").map(char => "●").join("");
+    
     res.status(200).json({
         status: true, 
-        message: 'User has sucessfully signed up!',
+        message: 'User has successfully signed up!',
         ...req.body,
         password: maskedPassword
     });
