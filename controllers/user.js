2c72282d1cac36212f667eec280a465fa1435888
Paige Knowles <paknowles@student.fullsail.edu>
2024-03-24 11:43:08 -0400

    update to user.js  (password split)

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,3 @@
-// const signup = (req, res) => {
-//     const maskedPassword = req.body.password.split("").map(char => "●").join("");
-//     res.status(200).json({
-//         status: true, 
-//         message: 'User has sucessfully signed up!',
-//         ...req.body,
-//         password: maskedPassword
-//     });
-// }
-
-// module.exports = { signup };
-
 const signup = (req, res) => {
     if (!req.body.password) {
         return res.status(422).json({
